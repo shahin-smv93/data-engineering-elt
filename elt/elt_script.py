457173533724a4529856375977b9fd7e7fa4ed33
shahinmasoumi93 <shahinmasoumi93@gmail.com>
2024-09-09 10:20:13 -0400

    elt script written

--- elt/elt_script.py
+++ elt/elt_script.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 
+# Check function
 def postgres_runs_first(host, max_tries=5, delay=5):
     tries = 0
     while tries < max_tries:
@@ -21,3 +22,49 @@ def postgres_runs_first(host, max_tries=5, delay=5):
             time.sleep(delay)
     print("Maximum tries reached. Exiting.")
     return False
+
+if not postgres_runs_first(host="source_postgres"):
+    exit(1)
+
+print("Starting ELT script")
+
+source_config = {
+    'dbname': 'source_db',
+    'user': 'postgres',
+    'password': 'secret',
+    'host': 'source_postgres'
+}
+
+destination_config = {
+    'dbname': 'destination_db',
+    'user': 'postgres',
+    'password': 'secret',
+    'host': 'destination_db'
+}
+
+dump_command = [
+    'pg_dump',
+    '-h', source_config['host'],
+    '-u', source_config['user'],
+    '-d', source_config['dbname'],
+    '-f', 'data_dump.sql',
+    '-w'
+]
+
+subprocess_env = dict(PGPASSWORD=source_config['password'])
+
+subprocess.run(dump_command, env=subprocess_env, check=True)
+
+load_command = [
+    'psql',
+    '-h', destination_config['host'],
+    '-u', destination_config['user'],
+    '-d', destination_config['dbname'],
+    '-a', '-f', 'data_dump.sql',
+]
+
+subprocess_env = dict(PGPASSWORD=destination_config['password'])
+
+subprocess.run(load_command, env=subprocess_env, check=True)
+
+print('End of the ELT script!')
